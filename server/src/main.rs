a6801380257f86c3cfde43962b31389347392ab8
pwbh <127856937+pwbh@users.noreply.github.com>
2023-06-20 18:44:01 +0300

    fooling around with colors in terminal

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    print_colored(
+        r"
+        ___ __  __ ____  
+        |_ _|  \/  |  _ \ 
+         | || |\/| | |_) |
+         | || |  | |  __/ 
+        |___|_|  |_|_|    
+    
+        Message queue server
+    ",
+        105,
+    )
+}
+
+fn print_colored(text: &str, color: usize) {
+    let t = format!("\x1b[38;5;{}m{}\x1b[0m", color, text);
+    println!("{}", t)
 }
