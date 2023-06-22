9a781f3fb22165d2828b5e80a71e7585ae377e2b
pwbh <127856937+pwbh@users.noreply.github.com>
2023-06-22 20:31:35 +0300

    Some more boilerplate for commands

--- observer/src/main.rs
+++ observer/src/main.rs
@@ -4,13 +4,15 @@ mod utils;
 
 fn main() {
     let hosts = parse_hosts();
-    println!("{:?}", hosts);
 
     let mut buf = String::new();
 
     loop {
         io::stdin().read_line(&mut buf).unwrap();
-        handle_command(buf.trim());
+        match handle_command(buf.trim()) {
+            Ok(status) => println!("\x1b[32m✓ {}\x1b[0m", status),
+            Err(e) => println!("\x1b[38;5;1mERROR:\x1b[0m {}", e),
+        };
         buf.clear();
     }
 }
@@ -30,12 +32,19 @@ fn handle_command(raw_command: &str) -> Result<String, String> {
     let mut tokens = raw_command.split_ascii_whitespace();
     let command = tokens.next().unwrap();
 
-    println!("Command executed: `{}`", command);
-
     match command {
         "CONNECT" => return handle_connect_command(&mut tokens),
         _ => return Err("unrecognized command has been passed.".to_string()),
     }
 }
 
-fn handle_connect_command(tokens: &mut SplitAsciiWhitespace<'_>) -> Result<String, String> {}
+fn handle_connect_command(tokens: &mut SplitAsciiWhitespace<'_>) -> Result<String, String> {
+    let hostname = match tokens.next() {
+        Some(hostname) => hostname,
+        None => return Err("hostname was not provided.".to_string()),
+    };
+
+    // do some logic for connecting
+
+    Ok("OK".to_string())
+}
