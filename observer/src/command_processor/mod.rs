1ba062fa5e9e65d3f6a4ae5a9c41c04f7abc67a5
pwbh <127856937+pwbh@users.noreply.github.com>
2023-06-28 10:19:39 +0300

    Did some changes to the main file

--- observer/src/command_processor/mod.rs
+++ observer/src/command_processor/mod.rs
@@ -1,6 +1,7 @@
-mod command;
+pub mod command;
 
-use self::command::{Command, CommandName};
+pub use self::command::Command;
+pub use self::command::CommandName;
 
 #[derive(Debug)]
 pub struct CommandProcessor {
@@ -12,37 +13,10 @@ impl CommandProcessor {
         Self { buf: String::new() }
     }
 
-    pub fn process_raw_command(&mut self) -> Result<String, String> {
+    pub fn process_raw_command(&mut self) -> Result<Command, String> {
         std::io::stdin().read_line(&mut self.buf).unwrap();
         let command = Command::from(&self.buf)?;
         self.buf.clear();
-        self.process_command(&command)
-    }
-
-    pub fn process_command(&self, command: &Command) -> Result<String, String> {
-        match command {
-            Command {
-                name: CommandName::Connect,
-                ..
-            } => return self.handle_connect_command(command),
-
-            _ => {
-                return Err(
-                    "unrecognized command has been passsed, please provide a relevant name."
-                        .to_string(),
-                )
-            }
-        }
-    }
-
-    fn handle_connect_command(&self, command: &Command) -> Result<String, String> {
-        let hostname = match command.arguments.iter().next() {
-            Some(hostname) => hostname,
-            None => return Err("hostname was not provided.".to_string()),
-        };
-
-        // do some logic for connecting
-
-        Ok("OK".to_string())
+        return Ok(command);
     }
 }
--- observer/src/main.rs
+++ observer/src/main.rs
@@ -1,7 +1,10 @@
 use clap::{arg, command};
 use std::{
+    collections::HashMap,
+    fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    path::PathBuf,
     thread::JoinHandle,
     time::Duration,
 };
@@ -11,16 +14,29 @@ mod utils;
 
 use command_processor::CommandProcessor;
 
-fn main() {
+const DEFAULT_CONFIG_PATH: &str = "./config/dev.properties";
+
+fn main() -> Result<(), String> {
     let matches = command!().arg(
         arg!(-f --follow <HOST> "Runs the Observer as a follower for leader located at <HOST>, Host MUST by booted without -f flag.")
         .required(false)
+    ).arg(arg!(-c --config <PATH> "Config file to use when starting Observer.")
+        .required(false)
+        .default_value(DEFAULT_CONFIG_PATH)
     ).get_matches();
 
     match matches.get_one::<String>("follow") {
         Some(host) => println!("Booting as a follower for {}", host),
         None => println!("Bootin as a leader"),
     };
+
+    let config_path = matches.get_one::<String>("config").unwrap();
+    let config = match load_config(config_path.into()) {
+        Ok(c) => c,
+        Err(e) => return Err(e),
+    };
+
+    println!("{:?}", config);
 
     let mut command_processor = CommandProcessor::new();
 
@@ -45,7 +61,14 @@ fn main() {
     // This will make sure our main thread will never exit until the user will issue an EXIT command by himself
     loop {
         match command_processor.process_raw_command() {
-            Ok(status) => println!("\x1b[32m✓ {}\x1b[0m", status),
+            Ok(command) => match command {
+                command_processor::Command {
+                    name: command_processor::CommandName::Connect,
+                    ..
+                } => handle_connect_command(&command),
+
+                _ => println!("sd"),
+            },
             Err(e) => println!("\x1b[38;5;1mERROR:\x1b[0m {}", e),
         };
     }
@@ -78,4 +101,53 @@ fn spawn_broker_stream_writer(mut stream: TcpStream) -> JoinHandle<()> {
         std::thread::sleep(Duration::from_millis(1500));
         stream.write("Hello from observer!\n".as_bytes()).unwrap();
     })
+}
+
+fn handle_connect_command(command: &command_processor::Command) {
+    let hostname = match command.arguments.iter().next() {
+        Some(hostname) => hostname,
+        None => {
+            println!("hostname was not provided.");
+            return;
+        }
+    };
+
+    // do some logic for connecting
+}
+
+#[derive(Debug)]
+enum Value {
+    String(String),
+    Number(i32),
+    Float(f32),
+}
+
+fn load_config(path: PathBuf) -> Result<HashMap<String, Value>, String> {
+    let mut config = HashMap::new();
+
+    let content = match fs::read_to_string(path) {
+        Ok(c) => c,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    for line in content.lines() {
+        let split: Vec<&str> = line.split_terminator("=").collect();
+
+        if split.len() != 2 {
+            return Err("property format is incorrect, should be key=value".to_string());
+        }
+
+        let key = split[0].to_string();
+        let value = if let Ok(f) = split[1].parse::<f32>() {
+            Value::Float(f)
+        } else if let Ok(i) = split[1].parse::<i32>() {
+            Value::Number(i)
+        } else {
+            Value::String(split[1].to_string())
+        };
+
+        config.entry(key).or_insert(value);
+    }
+
+    return Ok(config);
 }
