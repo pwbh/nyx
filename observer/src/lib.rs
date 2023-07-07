274277f0d77497c4154e551f344b9070df0400b0
pwbh <127856937+pwbh@users.noreply.github.com>
2023-07-07 15:12:52 +0300

    remove unncessary code from observer

--- observer/src/lib.rs
+++ observer/src/lib.rs
@@ -30,15 +30,8 @@ impl Observer {
     pub fn new(config_path: &str, role: Role) -> Result<Self, String> {
         let config = Config::from(config_path.into())?;
 
-        println!("{:?}", config);
-
-        let replica_factor = config
-            .get_number("replica_factor")
-            .ok_or("Replica factor is missing from config.".to_string())?;
-
-        println!("{}", replica_factor);
-
         let distribution_manager = DistributionManager::new(config);
+
         let command_processor = CommandProcessor::new();
 
         let listener = TcpListener::bind("localhost:3000").map_err(|e| e.to_string())?;
